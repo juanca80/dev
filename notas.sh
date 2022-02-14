6eb2205b698618725da48c44802af843cef1775a
Juan Lauri <jclauri.ponte@gmail.com>
2022-02-14 08:18:18 -0500

    Cambio de extension al archivo notas

--- notas.sh
+++ notas.sh
@@ -1,30 +1,38 @@
 # Comandos Git.
 # Inicializar repositorio Git
-git init
+$git init
 
 # Lo usamos para saber si tenemos un archivo añadido o borrado en nuestro proyecto, para saber en la rama en la que estamos y si tenemos commits.
-git status 
+$git status 
 
 # Es para añadir un archivo a nuestra rama seguidamente ponemos entre comillas el nombre de nuestro archivo o poner un punto para añadir todos los archivos de nuestra carpeta.
-git add . o <archivo> 
+$git add . o <archivo> 
 
 # Lo usamos para borrar un archivo que hayamos añadido, para eliminarlo por completo de nuestra rama usamosgit rm --cached.
-git rm  --cached <archivo>
+$git rm  --cached <archivo>
 
 # Se usa para añadir un commit a nuestra rama, también podemos ponerle un -m seguidamente ponemos entre comillas nuestro ensaje.
-git commit -m "Comentario"
+$git commit -m "Comentario"
 
 # Muestra configuraciones de git también podemos usar –list para mostrar la configuración por defecto de nuestro git y si añadimos --show-origin inhales nos muestra las configuraciones guardadas y su ubicación.
-git config --list --show-origin 
+$git config --list --show-origin 
 
 # Cambia de manera global el nombre del usuario, seguidamente ponemos entre comillas nuestro nombre.
-git config --global user.name "Juan Lauri"
+$git config --global user.name "Juan Lauri"
 
 # Cambia de manera global el email del usuario, seguidamente ponemos entre comillas nuestro nombre.
-git config --global user.email "jclauri.ponte@gmail.com"
+$git config --global user.email "jclauri.ponte@gmail.com"
 
 # Muestra todos los cambios històricos realizados
-git show
+$git show
 
 # Se usa para ver la historia de nuestros archivos, los commits, el usuario que lo cambió, cuando se realizaron los cambios etc. seguidamente ponemos el nombre de nuestro archivo.
-git log
+$git log
+
+# Muestra los cambios realizados entre el estado actual y el commit especificado por su HASH
+$git diff b37f461
+
+# Muestra los cambios realizados entre el commit1 y commit2 especificado por su HASH
+$git diff 5f6c11d b37f461
+
+# 
